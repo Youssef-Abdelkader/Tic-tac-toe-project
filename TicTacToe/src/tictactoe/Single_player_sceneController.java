27ebd688d1cc26c86ef4b36d2003846e02e1a35a
Abdelrahman Atia <abdelrahmanatia745@gmail.com>
2025-01-07 15:18:02 +0200

    add a controller class for single player secene

---
+++ TicTacToe/src/tictactoe/Single_player_sceneController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author A.Atia
+ */
+
+public class Single_player_sceneController extends Single_Player_Scene{
+
+    public Single_player_sceneController(Stage stage) {
+        super(stage);
+    }
+
+   
+}
