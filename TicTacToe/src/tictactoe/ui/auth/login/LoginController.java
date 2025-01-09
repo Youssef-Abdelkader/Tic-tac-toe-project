c0040903cdce57a62237d53ee1fb9259ee48e188
Luka <malak.raaof664@gmail.com>
2025-01-09 23:02:20 +0200

    FIRST SUCCESSFUL CONNECTION BETWEEN CLIENT & SERVER

--- TicTacToe/src/tictactoe/ui/auth/login/LoginController.java
+++ TicTacToe/src/tictactoe/ui/auth/login/LoginController.java
@@ -5,6 +5,13 @@
  */
 package tictactoe.ui.auth.login;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -19,8 +26,48 @@ import tictactoe.ui.home.online.HomeOnlineController;
  */
 public class LoginController extends Login {
     
+    Socket server;
+    DataInputStream ear;
+    PrintStream mouth;
+    
+    
     public LoginController(Stage stage) {
         super(stage);
+        
+        try {
+            server = new Socket("127.0.0.1", 5005);
+            ear = new DataInputStream(server.getInputStream());
+            mouth = new PrintStream(server.getOutputStream());
+
+            // Start a thread to listen for messages from the server
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                           String user = ear.readLine();
+                            if (user != null) {
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        System.out.println(user);
+                                    }
+                                });
+                            }
+                        }
+                    } catch (IOException ex) {
+                        Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }).start();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+       
+        
+        
         signUp.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -36,6 +83,18 @@ public class LoginController extends Login {
                 HomeOnlineController home = new HomeOnlineController(stage);
                 Scene scene = new Scene(home);
                 stage.setScene(scene);
+                
+                
+                
+                
+                //*************************
+                
+                String user = nameText.getText();
+                if (!user.isEmpty()) {
+                    //txta.appendText("You: " + message + "\n");
+                    mouth.println(user);
+                   
+            }
             }
         });
         
